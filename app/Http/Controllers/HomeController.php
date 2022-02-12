265ff87748afa0286a76141553867460ca4b2af9
Maryanto Cin <firevolt719@gmail.com>
2022-02-12 22:06:39 +0700

    feat: added middleware

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,22 +15,12 @@ use Illuminate\Support\Facades\Redirect;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function index()
     {
-        $this->middleware('auth');
+        return view('welcome');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
+    public function home()
     {
         $ebooks = Ebook::all();
         return view('home', ['ebooks' => $ebooks]);
--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/profile', [App\Http\Controllers\HomeController::class, 'show_profile'])->name('show_profile');
-Route::patch('/profile', [App\Http\Controllers\HomeController::class, 'update_profile'])->name('update_profile');
-Route::get('/ebook/{ebook}', [App\Http\Controllers\HomeController::class, 'show_ebook'])->name('show_ebook');
-ROute::post('/ebook/{ebook}', [App\Http\Controllers\HomeController::class, 'rent_ebook'])->name('rent_ebook');
-Route::get('/cart', [App\Http\Controllers\HomeController::class, 'show_cart'])->name('show_cart');
-Route::delete('/order/{order}', [App\Http\Controllers\HomeController::class, 'delete_cart'])->name('delete_cart');
-Route::post('/checkout', [App\Http\Controllers\HomeController::class, 'checkout_cart'])->name('checkout_cart');
-Route::get('/account', [App\Http\Controllers\HomeController::class, 'show_account'])->name('show_account');
-Route::get('/account/{account}', [App\Http\Controllers\HomeController::class, 'show_update_role'])->name('show_update_role');
-Route::patch('/account/{account}', [App\Http\Controllers\HomeController::class, 'update_role'])->name('update_role');
-Route::delete('/account/{account}', [App\Http\Controllers\HomeController::class, 'delete_account'])->name('delete_account');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
+    Route::get('/profile', [App\Http\Controllers\HomeController::class, 'show_profile'])->name('show_profile');
+    Route::patch('/profile', [App\Http\Controllers\HomeController::class, 'update_profile'])->name('update_profile');
+    Route::get('/ebook/{ebook}', [App\Http\Controllers\HomeController::class, 'show_ebook'])->name('show_ebook');
+    ROute::post('/ebook/{ebook}', [App\Http\Controllers\HomeController::class, 'rent_ebook'])->name('rent_ebook');
+    Route::get('/cart', [App\Http\Controllers\HomeController::class, 'show_cart'])->name('show_cart');
+    Route::delete('/order/{order}', [App\Http\Controllers\HomeController::class, 'delete_cart'])->name('delete_cart');
+    Route::post('/checkout', [App\Http\Controllers\HomeController::class, 'checkout_cart'])->name('checkout_cart');
+});
+
+Route::middleware(['admin'])->group(function () {
+    Route::get('/account', [App\Http\Controllers\HomeController::class, 'show_account'])->name('show_account');
+    Route::get('/account/{account}', [App\Http\Controllers\HomeController::class, 'show_update_role'])->name('show_update_role');
+    Route::patch('/account/{account}', [App\Http\Controllers\HomeController::class, 'update_role'])->name('update_role');
+    Route::delete('/account/{account}', [App\Http\Controllers\HomeController::class, 'delete_account'])->name('delete_account');
+});
